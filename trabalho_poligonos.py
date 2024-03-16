e142e043e15bafb002658c8cd03c204f4263cc28
Vitor Mattos <vitorfmattos03@gmail.com>
2024-03-16 20:34:11 -0300

    Organizando de modo a respeitar os paradigmas de orientação a objetos

--- trabalho_poligonos.py
+++ trabalho_poligonos.py
@@ -1,99 +1,135 @@
-import math
-from colorama import init, Fore
+import math #Biblioteca para cálculos matemáticos
+from colorama import init, Fore #Biblioteca para adicionar cores ao programa
 
+#######################################################################
+
+#SuperClasse Polygon
 
 class Polygon:
-    
-    
+    #Construtor
     def __init__(self):
-        
- 
+        pass
     
-        def calcular_area(self, altura, comprimento):
-            raise NotImplementedError("Método não implementado na classe filha")
-            
-        def calcular_volume(self, altura, comprimento, largura):
-            raise NotImplementedError("Método não implementado na classe filha")
-        
-        
+    """
+    Método responsável por capturar os inputs do usuário.
+    Este método também valida o tipo de dado de input.
+    A mensagem do parâmetro é passada por argumento nas classes filhas.
+    """
+    def get_input(self, message):
+        while True:
+            try:
+                value = float(input(message))
+                if value <= 0:
+                    print(Fore.RED + "O valor deve ser positivo.")
+                else:
+                    return value
+            except ValueError:
+                print(Fore.RED + "Por favor, insira um número válido.")
 
-class Quadrilatero(Polygon):
-    def __init__(self):
-        super().__init__()
+    """
+    Este método recebe uma mensagem (vinda por argumento no método main) e concatena com o resultado obtido 
+    dos métodos das classes filhas
+    """
+    def display_result(self, result_message, result):
+        print(Fore.CYAN + result_message, result)
+
+#Métodos a serem implementados nas classes filhas
+        
+    def calcular_area(self):
+        raise NotImplementedError("Método não implementado na classe filha")
+            
+    def calcular_volume(self):
+        raise NotImplementedError("Método não implementado na classe filha")   
     
-    def calcular_area(self, altura, comprimento):
-        return altura*comprimento
+################################################################################################
+
+#Classes FILHAS:
+
+"""
+    As classes filhas implementam os métodos calcular_area e calcular_volume.
+    Obtém os inputs através do método get_input vindo da superclasse.
+
+"""
+class Quadrilatero(Polygon):
+    def calcular_area(self):
+        altura = self.get_input(Fore.YELLOW + "Digite a altura do quadrilátero: ")
+        comprimento = self.get_input("Digite o comprimento do quadrilátero: ")
+        return altura * comprimento
         
         
 class Triangulo(Polygon):
-    def __init__(self):
-        super().__init__()
-    
-    def calcular_area(self, altura, comprimento):
-        return (altura*comprimento) / 2
+    def calcular_area(self):
+        altura = self.get_input(Fore.YELLOW + "Digite a altura do triângulo: ")
+        comprimento = self.get_input("Digite o comprimento da base do triângulo: ")
+        return (altura * comprimento) / 2
         
         
 class Circulo(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_area(self, raio):
+    def calcular_area(self):
+        raio = self.get_input(Fore.YELLOW + "Digite o raio do círculo: ")
         return raio*raio*math.pi
         
         
 class Pentagono(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_area(self, comprimento, altura):
+    def calcular_area(self):
+        altura = self.get_input(Fore.YELLOW + "Digite a altura do pentagono: ")
+        comprimento = self.get_input("Digite o comprimento da base do pentagono: ")
         return 5*comprimento*altura
         
 class Hexagono(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_area(self, comprimento):
+    def calcular_area(self):
+        comprimento = self.get_input(Fore.YELLOW + "Digite o comprimento do lado do hexágono: ")
         return (3*math.sqrt(3)*math.pow(comprimento,2))/2
         
 
 class Paralelepipedo(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_volume(self, comprimento, altura, largura):
+    def calcular_volume(self):
+        comprimento = self.get_input(Fore.YELLOW + "Digite o comprimento do paralelepípedo: ")
+        altura = self.get_input("Digite a altura do paralelepípedo: ")
+        largura = self.get_input("Digite a largura do paralelepípedo: ")
         return comprimento * altura * largura
 
 class Piramide(Polygon):
-    def __init__(self):
-        self.quadrilatero = Quadrilatero()
-        super().__init__()
     
-    def calcular_volume(self, altura, comprimento, largura):
-        return (1/3) * self.quadrilatero.calcular_area(comprimento,largura) * altura;
+    def calcular_volume(self):
+        altura = self.get_input(Fore.YELLOW + "Digite a altura da pirâmide: ")
+        comprimento = self.get_input("Digite o comprimento da base da pirâmide: ")
+        largura = self.get_input("Digite a largura da base da pirâmide: ")
+        return (1/3) * (comprimento * largura) * altura
 
 class Cilindro(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_volume(self, raio, altura):
+    def calcular_volume(self):
+        raio = self.get_input(Fore.YELLOW + "Digite o raio do cilindro: ")
+        altura = self.get_input("Digite a altura do cilindro: ")
         return math.pi * math.pow(raio,2) * altura
 
 
 class Cone(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_volume(self, raio, altura):
+    def calcular_volume(self):
+        raio = self.get_input(Fore.YELLOW + "Digite o raio do cone: ")
+        altura = self.get_input("Digite a altura do cone: ")
         return ((1/3) * math.pi) * math.pow(raio,2) * altura
 
 
 class Esfera(Polygon):
-    def __init__(self):
-        super().__init__()
     
-    def calcular_volume(self, raio):
+    def calcular_volume(self):
+        raio = self.get_input(Fore.YELLOW + "Digite o raio da esfera: ")
         return ((4/3) * math.pi) * math.pow(raio,3)
+    
+##############################################################################
 
+#Método MENU 
+
+"""
+Obtém o input para escolha do polígono
+"""
 def menu():
     
     print(Fore.YELLOW + "Escolha um polígono:\n")
@@ -114,6 +150,17 @@ def menu():
     except ValueError:
         print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
 
+#####################################################################################
+        
+#Método MAIN
+        
+"""
+Analisa a escolha do usuário (entre as opções de polígonos).
+Instancia os objetos correspondentes a escolha.
+Obtém o resultado da área ou volume através dos métodos nas classes filhas.
+Exibe o resultado.
+Repete a operação até que o usuário digite '0' ou 'q'
+"""
 
 def main():
     escolha = ...
@@ -123,83 +170,54 @@ def main():
         escolha = menu()
 
         if escolha == 1:
-            try:
-                altura = float(input(Fore.YELLOW + "Digite a altura do quadrilátero: "))
-                comprimento = float(input("Digite o comprimento do quadrilátero: "))
-                print(Fore.CYAN + "Área do quadrilátero:", Quadrilatero().calcular_area(altura, comprimento))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            quadrilatero = Quadrilatero()
+            area = quadrilatero.calcular_area()
+            quadrilatero.display_result("Área do quadrilátero:", area)
 
         elif escolha == 2:
-            try:
-                altura = float(input(Fore.YELLOW + "Digite a altura do triângulo: "))
-                comprimento = float(input("Digite o comprimento da base do triângulo: "))
-                print(Fore.CYAN + "Área do triângulo:", Triangulo().calcular_area(altura, comprimento))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            triangulo = Triangulo()
+            area = triangulo.calcular_area()
+            triangulo.display_result("Área do triângulo:", area)
 
         elif escolha == 3:
-            try:
-                raio = float(input(Fore.YELLOW +"Digite o raio do círculo: "))
-                print(Fore.CYAN + "Área do círculo:", Circulo().calcular_area(raio))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            circulo = Circulo()
+            area = circulo.calcular_area()
+            circulo.display_result("Área do círculo:", area)
 
         elif escolha == 4:
-            try:
-                comprimento = float(input(Fore.YELLOW +"Digite o comprimento do pentágono: "))
-                altura = float(input("Digite a altura do pentágono: "))
-                print(Fore.CYAN + "Área do pentágono:", Pentagono().calcular_area(comprimento, altura))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            pentagono = Pentagono()
+            area = pentagono.calcular_area()
+            pentagono.display_result("Área do pentágono:", area)
 
         elif escolha == 5:
-            try:
-                lado = float(input(Fore.YELLOW +"Digite o comprimento do lado do hexágono: "))
-                print(Fore.CYAN + "Área do hexágono:", Hexagono().calcular_area(lado))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            hexagono = Hexagono()
+            area = hexagono.calcular_area()
+            hexagono.display_result("Área do hexágono:", area)
 
         elif escolha == 6:
-            try:
-                comprimento = float(input(Fore.YELLOW +"Digite o comprimento do paralelepípedo: "))
-                altura = float(input("Digite a altura do paralelepípedo: "))
-                largura = float(input("Digite a largura do paralelepípedo: "))
-                print(Fore.CYAN + "Volume do paralelepípedo:", Paralelepipedo().calcular_volume(comprimento, altura, largura))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            paralelepipedo = Paralelepipedo()
+            volume = paralelepipedo.calcular_volume()
+            paralelepipedo.display_result("Volume do paralelepípedo:", volume)
 
         elif escolha == 7:
-            try:
-                altura = float(input(Fore.YELLOW +"Digite a altura da pirâmide: "))
-                comprimento = float(input("Digite o comprimento da base da pirâmide: "))
-                largura = float(input("Digite a largura da base da pirâmide: "))
-                print(Fore.CYAN + "Volume da pirâmide:", Piramide().calcular_volume(altura, comprimento, largura))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            piramide = Piramide()
+            volume = piramide.calcular_volume()
+            piramide.display_result("Volume da pirâmide:", volume)
 
         elif escolha == 8:
-            try:
-                raio = float(input(Fore.YELLOW +"Digite o raio do cilindro: "))
-                altura = float(input("Digite a altura do cilindro: "))
-                print(Fore.CYAN + "Volume do cilindro:", Cilindro().calcular_volume(raio, altura))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            cilindro = Cilindro()
+            volume = cilindro.calcular_volume()
+            cilindro.display_result("Volume do cilindro:", volume)
 
         elif escolha == 9:
-            try:
-                raio = float(input(Fore.YELLOW +"Digite o raio do cone: "))
-                altura = float(input("Digite a altura do cone: "))
-                print(Fore.CYAN + "Volume do cone:", Cone().calcular_volume(raio, altura))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            cone = Cone()
+            volume = cone.calcular_volume()
+            cone.display_result("Volume do cone:", volume)
 
         elif escolha == 10:
-            try:
-                raio = float(input(Fore.YELLOW +"Digite o raio da esfera: "))
-                print(Fore.CYAN + "Volume da esfera:", Esfera().calcular_volume(raio))
-            except ValueError:
-                print(Fore.RED + "Desculpe, você não inseriu um número inteiro válido.")
+            esfera = Esfera()
+            volume = esfera.calcular_volume()
+            esfera.display_result("Volume da esfera:", volume)
         
         elif escolha == 0:
             print(Fore.RED + "\nBye bye!\n")
@@ -214,6 +232,12 @@ def main():
             print("\nBye bye!\n")
             Fore.RESET
             break
+
+
+"""
+Como o programa é executado diretamente (e não chamado como um módulo),
+o método main() será chamado logo de início
+"""
 
 if __name__ == "__main__":
     main()
